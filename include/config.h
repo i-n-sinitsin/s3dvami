1b19ad2c2155b28ab287dd43e444875773f8ec49
Sinitsin Ivan <i.sinitsin@octaviangaming.com>
2022-07-02 00:16:55 +0300

    add config file

---
+++ include/config.h
@@ -0,0 +1,10 @@
+// Copyright © 2022 Sinitsin Ivan, Contacts: <i.n.sinitsin@gmail.com>
+
+#pragma once
+
+namespace s3dvami
+{
+    constexpr size_t defaultWindowWidth = 1024;
+    constexpr size_t defaultWindowHeight = 768;
+    // constexpr glm::vec4 defautBackgroundColor = {0.5f, 0.5f, 0.5f, 1.0f};
+}
