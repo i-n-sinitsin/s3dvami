b348013505a4ec49e2fc762e9d7d95f576e2e3a3
Sinitsin Ivan <i.sinitsin@octaviangaming.com>
2022-07-08 16:37:29 +0300

    add simple shader

---
+++ include/shaders/simple_vert.h
@@ -0,0 +1,26 @@
+// Copyright © 2022 Sinitsin Ivan, Contacts: <i.n.sinitsin@gmail.com>
+
+#pragma once
+
+#include <string>
+
+namespace s3dvami
+{
+    static const std::string simple_vert = R"VERT(
+#version 430 core
+
+layout(location = 0) in vec3 a_pos;
+layout(location = 1) in vec3 a_norm;
+layout(location = 2) in vec2 a_tex;
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+
+void main()
+{
+    mat4 pvm = u_projection * u_view * u_model;
+    gl_Position = pvm * vec4(a_pos, 1.0f);
+}
+)VERT";
+}
