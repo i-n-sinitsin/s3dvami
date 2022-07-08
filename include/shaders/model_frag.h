ad19f255047109ee2e11e74f37aef908a098fd46
Sinitsin Ivan <i.sinitsin@octaviangaming.com>
2022-07-08 07:54:18 +0300

    add simple shaders for model

---
+++ include/shaders/model_frag.h
@@ -0,0 +1,20 @@
+// Copyright © 2022 Sinitsin Ivan, Contacts: <i.n.sinitsin@gmail.com>
+
+#pragma once
+
+#include <string>
+
+namespace s3dvami
+{
+    static const std::string model_frag = R"
+        #version 430 core
+
+        precision mediump float;
+        out vec4 v_color;
+
+        void main()
+        {
+            v_color = vec4(1, 1, 1, 1);
+        }
+    ";
+}
