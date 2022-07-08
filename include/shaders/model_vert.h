6163d8633bc79aca35c8efcd49c69f6dd95d3099
Sinitsin Ivan <i.sinitsin@octaviangaming.com>
2022-07-08 16:37:42 +0300

    fix model shader

--- include/shaders/model_vert.h
+++ include/shaders/model_vert.h
@@ -12,6 +12,7 @@ namespace s3dvami
 layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec3 a_norm;
 layout(location = 2) in vec2 a_tex;
+
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
