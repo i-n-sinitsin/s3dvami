5ec8d2318521a8828bf3b19616309630983f5512
Ivan Sinitsin <i.n.sinitsin@gmail.com>
2022-07-11 20:30:41 +0300

    implement load texture to gl

--- src/model/texture.cpp
+++ src/model/texture.cpp
@@ -5,15 +5,45 @@
 namespace s3dvami
 {
 
-    Texture::Texture(const std::string & /*id*/, const std::vector<uint8_t> & /*rawData*/, unsigned int /*width*/, unsigned int /*height*/)
-    {}
+    Texture::Texture(const std::string &id, const std::vector<uint8_t> &rawData, unsigned int width, unsigned int height)
+        : m_glId(0)
+        , m_id(id)
+        , m_width(width)
+        , m_height(height)
+    {
+        glGenTextures(1, &m_glId);
+        glBindTexture(GL_TEXTURE_2D, m_glId);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &rawData[0]);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
 
     Texture::~Texture()
-    {}
+    {
+        glDeleteTextures(1, &m_glId);
+    }
 
     std::string Texture::id() const
     {
-        return "";
+        return m_id;
+    }
+
+    unsigned int Texture::width() const
+    {
+        return m_width;
+    }
+
+    unsigned int Texture::height() const
+    {
+        return m_height;
     }
 
 }
