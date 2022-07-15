2010d6c9ca70f26c9f33a507f83bd217cebf1dfe
Ivan Sinitsin <i.n.sinitsin@gmail.com>
2022-07-15 16:04:35 +0300

    update "model" shader

--- include/shaders/model_vert.h
+++ include/shaders/model_vert.h
@@ -13,6 +13,7 @@ layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec3 a_norm;
 layout(location = 2) in vec2 a_tex;
 
+// transformations
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
