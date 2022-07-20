f573b77784c29afd787133d993358ce1c592dd2e
Ivan Sinitsin <i.n.sinitsin@gmail.com>
2022-07-20 08:42:44 +0300

    add ortho projection (#61)

--- src/camera.cpp
+++ src/camera.cpp
@@ -6,6 +6,7 @@
 
 namespace s3dvami
 {
+
     Camera::Camera()
         : m_projection(1.0f)
         , m_view(1.0f)
@@ -24,6 +25,26 @@ namespace s3dvami
     void Camera::setPerspectiveProjection(float fov, float width, float height, float near, float far)
     {
         m_projection = glm::perspective(glm::radians(fov), width / height, near, far);
+    }
+
+    void Camera::setOrthoProjection(float left, float right, float bottom, float top, float near, float far)
+    {
+        m_projection = glm::ortho(left, right, bottom, top, near, far);
+    }
+
+    void Camera::setOrthoProjection(float width, float height, float depth)
+    {
+        setOrthoProjection(-width / 2.0f, width / 2.0f, -height / 2.0f, height / 2.0f, 0.01f, depth);
+    }
+
+    void Camera::setOrtho2DProjection(float left, float right, float bottom, float top)
+    {
+        m_projection = glm::ortho(left, right, bottom, top);
+    }
+
+    void Camera::setOrtho2DProjection(float width, float height)
+    {
+        setOrtho2DProjection(-width / 2.0f, width / 2.0f, -height / 2.0f, height / 2.0f);
     }
 
     void Camera::setView(glm::vec3 eye, glm::vec3 center, glm::vec3 up)
